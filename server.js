815bbd56e2ec87caa944a86cddf9a89bac7c0ea6
Apoorva Sharma <apsharma@adobe.com>
2020-05-26 21:39:53 +0530

    update for post call

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const cp = require("cookie-parser");
 const app = express();
-const port = 4000;
 
 app.use(cp());
 app.use(express.static("public"));
 
 app.enable("trust proxy");
-// app.use(function(req, res, next) {
-//   if (req.secure) {
-//     return next();
-//   }
-// //   res.redirect("https://" + req.headers.host + req.url);
-// });
+app.use(function(req, res, next) {
+  if (req.secure) {
+    return next();
+  }
+//   res.redirect("https://" + req.headers.host + req.url);
+});
 
 app.get("/", function(request, response) {
 
@@ -25,7 +24,7 @@ app.get("/", function(request, response) {
   response.cookie("teststrict", "vl05", { sameSite: "strict" });
   response.set(
     "Access-Control-Allow-Origin",
-    "http://localhost:3100"
+    "https://git.corp.adobe.com"
   );
   response.set("Access-Control-Allow-Credentials", "true");
   response.json(request.cookies);
@@ -34,7 +33,7 @@ app.get("/", function(request, response) {
 app.all("/cookies.json", function(request, response) {
   response.set(
     "Access-Control-Allow-Origin",
-    "http://localhost:3100"
+    "https://git.corp.adobe.com"
   );
   response.set("Access-Control-Allow-Credentials", "true");
   response.json(request.cookies);
@@ -62,23 +61,56 @@ app.get("/embed", function(request, response) {
         sameSite: "none",
         secure: true
       });
-    response.sendFile(__dirname + "/views/embed.html");
+   response.sendFile(__dirname + "/views/embed.html");
   });
 
-app.get("/post", function(request, response) {
-response.cookie("plain-cookie", "plain");
-response.cookie("samesite-none", "none", { sameSite: "none", secure: true });
-response.cookie("samesite-lax", "lax", { sameSite: "lax" });
-response.cookie("samesite-strict", "strict", { sameSite: "strict" });
-response.sendFile(__dirname + "/views/post.html");
+app.get("/samesiteLax", function(request, response) {
+    response.cookie("samesite-lax", "testlax", { sameSite: "lax" });
+    response.sendFile(__dirname + "/views/view.html");
 });
 
-app.post("/callback", function(request, response) {
-    response.sendFile(__dirname + "/views/callback.html");
+app.get("/samesiteNone", function(request, response) {
+    response.cookie("samesite-none-only", "test", { sameSite: "none" });
+    response.sendFile(__dirname + "/views/view.html");
+});
+
+app.get("/samesitestrict", function(request, response) {
+    response.cookie("samesite-strict", "teststrict", { sameSite: "strict" });
+    response.sendFile(__dirname + "/views/view.html");
+});
+
+app.post('/post', function(request, response) {
+  response.set(
+  "Access-Control-Allow-Origin",
+  "https://git.corp.adobe.com"
+);
+response.set("Access-Control-Allow-Credentials", "true");
+response.setHeader(
+  "set-cookie",
+  "samesite-invalid=test; SameSite=InvalidValue"
+);
+response.cookie("samesite-not-set", "test");
+console.log(request.cookies);
+response.sendFile(__dirname + '/views/iframe.html');
+});
+
+app.get("/home", function(request, response) {
+  response.setHeader(
+    "set-cookie",
+    "samesite-invalid=test; SameSite=InvalidValue"
+  );
+  response.cookie("samesite-home", "test");
+  response.cookie("samesite-none-secure-home", "test", {
+    sameSite: "none",
+    secure: true
   });
-
-
-app.listen(port, function() {
-    console.log(`Listening to requests on http://localhost:${port}`);
+  response.cookie("samesite-none-only-home", "test", { sameSite: "none" });
+  response.cookie("samesite-lax", "test", { sameSite: "lax" });
+  response.cookie("samesite-strict", "test", { sameSite: "strict" });
+  response.sendFile(__dirname + "/views/test.html");
 });
 
+
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
